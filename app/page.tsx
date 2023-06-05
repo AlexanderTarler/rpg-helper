de4d3df86c952f71c07b596c4c60def239601207
alex <alexander.tarler@gmail.com>
2023-06-05 18:53:43 +0200

    Removed the temporary background and started to organize each page depending on their function

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 import './globals.css';
 import Title from './Title';
-import Background from './Background';
 
 
 export default function Home() {
   return (
     <>
       <Title title={'Awesome RPG project!'} />
-      <Background />
+      <div id="home">
+        Info about the latest campaign, latest notes and pinned things should go here.
+      </div>
     </>
   )
 }
